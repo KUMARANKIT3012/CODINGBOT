2311378e88c041964a09ae5b42c1fa754f81aeaa
Ankit Kumar <ankitkumar16729@gmail.com>
2024-10-11 20:03:28 +0530

    Add files via upload

---
+++ programming1.cpp
@@ -0,0 +1,49 @@
+//hello world program
+/*
+#include<iostream>//header file
+using namespace std;//standard namespace c++
+int main(){//start
+    cout<<"hello world";
+    return 0;
+}
+*/
+
+// calculate simple interest
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int p;
+    int r;
+    int t;
+    float si;
+
+    cin>>p;
+    cin>>r;
+    cin>>t;
+    si=p*r*t/100.0;
+    cout<<si;
+    cout<<'\n';
+    return 0;
+}
+*/
+
+// using operators 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if((a>=0)&&(a<=10)){
+        cout<<"Hello"<<endl;//endl=='\n'
+    }
+    else if((a>10)&&(a<=20)){
+        cout<<"world"<<endl;
+    }
+    else{
+        cout<<"welcome to coding blocks"<<endl;
+    }
+    return 0;
+}
+*/
